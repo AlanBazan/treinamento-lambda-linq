7c326767b6ecb27b92edcde19b80b6108ee5a7ad
Alan Bazan <alan.bazan@engenhos.net>
2023-02-03 09:21:27 -0300

    Projeto inicial

---
+++ Solution1/Delegates/Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Delegates
+{
+    public class Calculadora
+    {
+        public double Resultado { get; private set; }
+
+        public double valor;
+
+        public void SetValor(double valor)
+        {
+
+        }
+
+        public void Somar()
+        {
+
+        }
+
+        public void Subtrair()
+        {
+
+        }
+
+        public void EfetuarOperacao()
+        {
+
+        }
+
+    }
+}
---
+++ Solution1/Delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static double Somar(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
